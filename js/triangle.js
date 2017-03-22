539000dfcc1c7cecb9ffae15479368dc9e82e3ad
Mateusz Smykowski <msmykowski@interia.pl>
2017-03-22 20:03:27 +0100

    Area of a triangle JS script

---
+++ js/triangle.js
@@ -0,0 +1,8 @@
+var a = prompt('Podaj długość podstawy trójkąta a:'),
+    h = prompt('Podaj wysokość trójkąta h:'),
+    triangleArea;
+
+triangleArea = a * h / 2;
+
+alert('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi ' + triangleArea + ' jednostek kwadratowych.');
+console.log('Pole trójkąta o podstawie a = ' + a + ' i wysokości h = ' + h + ' wynosi ' + triangleArea + ' jednostek kwadratowych.');
